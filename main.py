83165b69057aa7823b9cf9086c1fd37615c668be
Payam Gh <payam.gh90@yahoo.co.uk>
2024-08-08 12:52:42 +0100

    initialize the route to the home page (test)

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ server = Flask(__name__, static_folder='static', template_folder='templates')
 
 if __name__ == "__main__":
     server.run(debug=True)
+    
+@server.rout("/")
+def index():
+    return render_template('front-end/src/main.jsx')
+
+# this is a test
