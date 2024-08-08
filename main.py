f6ea70eded798d871350a558f1ad5c243e7e288e
Payam Gh <payam.gh90@yahoo.co.uk>
2024-08-08 13:17:33 +0100

    uploading backend files on experimental

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ server = Flask(__name__, static_folder='front-end\src\assets', template_folder='
 def index():
     return render_template('/index.html')
 
-# @server.route('/<path:path>')
-# def static_proxy(path):
-#     file_path = os.path.join(server.static_folder, path)
-#     if os.path.exists(file_path):
-#         return send_from_directory(server.static_folder, path)
-#     else:
-#         return render_template('index.html')
 
 
 if __name__ == "__main__":
