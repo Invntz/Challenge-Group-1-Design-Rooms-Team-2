23f3eeb6368c81f78a403407cfb382e2ce8308b5
Payam Gh <payam.gh90@yahoo.co.uk>
2024-08-08 13:13:04 +0100

    trying to reach index.html

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-server = Flask(__name__, static_folder='static', template_folder='templates')
+server = Flask(__name__, static_folder='front-end\src\assets', template_folder='front-end')
+
+    
+@server.route("/")
+def index():
+    return render_template('/index.html')
+
+# @server.route('/<path:path>')
+# def static_proxy(path):
+#     file_path = os.path.join(server.static_folder, path)
+#     if os.path.exists(file_path):
+#         return send_from_directory(server.static_folder, path)
+#     else:
+#         return render_template('index.html')
+
 
 if __name__ == "__main__":
     server.run(debug=True)
-    
-@server.rout("/")
-def index():
-    return render_template('front-end/src/main.jsx')
-
 # this is a test
