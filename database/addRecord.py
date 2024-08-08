a0a844de8d119b7aa91b3a2fe93071f4a6f09eaf
Payam Gh <payam.gh90@yahoo.co.uk>
2024-08-08 16:16:55 +0100

    created add add new user

--- database/addRecord.py
+++ database/addRecord.py
@@ -1,4 +1,32 @@
-from connect import *
+<<<<<<< HEAD
+# this is the Sign Up page
+=======
+>>>>>>> 40d2b029160a7ad990a02ccc02edf94a4b6601a4
+import db_server
 
-def insert_record():
-    pass
+data_base = db_server.data_base
+admin = db_server.admin
+
+def add_new_user():
+    new_user = []
+    #user_id is autoincremented
+    first_name = input("Enter film title: ")
+    last_name = int(input("Enter year released: "))
+    username = input("Choose a unique username: ")
+    password = input("Choose a strong password: ")
+    bio = input ("Tell others about yourself: ")
+    # date_joined = date_joined()
+    role = input("Which one describes you? 'Customer','Designer' or Manufacturer'?")
+    new_user = new_user + [first_name, last_name, username, password, bio, role]
+
+    try:
+        admin.execute("INSERT INTO customers (first_name, last_name, username, password, bio, role) VALUES(?,?,?,?,?,?)", new_user)
+        data_base.commit()
+        print(f"\n{new_user} added to the customers table.")
+
+    except db_server.sql.OperationalError as e:
+        data_base.rollback()
+        print(f"\nRecord not added: {e}")
+
+if __name__ == "__main__":
+    add_new_user()
