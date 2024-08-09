63dd7821afdf45a375dcf76fa82964c0ed248221
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2024-08-09 14:47:03 +0100

    Updated create table

--- database/create_tables.py
+++ database/create_tables.py
@@ -3,13 +3,14 @@ import db_server
 try:
     # Create the table
     db_server.data_base.execute("""
-    CREATE TABLE user_info (
+    CREATE TABLE IF NOT EXISTS user_info (
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_name TEXT NOT NULL,
         first_name TEXT NOT NULL,
         last_name TEXT NOT NULL,
         email TEXT NOT NULL,
-        password TEXT NOT NULL,
+        hashed_password TEXT NOT NULL,
+        salt TEXT NOT NULL,
         token_invalidator TEXT,
         bio TEXT,
         date_joined DATE,
@@ -20,6 +21,23 @@ try:
         profile_img TEXT
     )""")
 
-
+ # Create the purchase table
+    db_server.data_base.execute("""
+    CREATE TABLE IF NOT EXISTS purchase (
+        purchase_id INT,
+        user_id INT,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(100),
+        order_num INT,
+        qty_purchased INT,
+        items_purchased TEXT,
+        date_purchased DATETIME,
+        brand VARCHAR(50),
+        basket_price DECIMAL(10, 2)
+    )
+    """)
+    
+    
 except Exception as e:
     print(f"An error occurred: {e}")
