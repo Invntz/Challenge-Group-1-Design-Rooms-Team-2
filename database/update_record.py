5f13f292224c5a1b0bca743ae841cdb17b283dc6
Payam Gh <payam.gh90@yahoo.co.uk>
2024-08-09 14:57:17 +0100

    Payam - add new user is now tested and it is functional

--- database/update_record.py
+++ database/update_record.py
@@ -8,13 +8,16 @@ def update_record():
     selected_table = input("Are you updating details of a 'USER' or 'PRODUCT'? ")
     
     if selected_table.lower() == "user":
+    
+        selected_record = input("What is the USER ID of the record? ")
         selected_field = input("What are you updating? 'First Name', 'Last Name', 'Bio'? ")
+        new_value = input("What is the NEW VALUE for the item? ")
         
         repeat = True
         while repeat:
+            
             if selected_table.lower() == "first name":
                 selected_table == "first_name"
-                break
             
             elif selected_table.lower() == "last name":
                 selected_table == "last_name"
@@ -29,24 +32,25 @@ def update_record():
             
             
         try:
-            admin.execute(f"UPDATE customer_info SET {selected_field} = ? WHERE filmid = ?", (new_value, film_id))
-            database.commit()
-            print(f"\nRecord {film_id} has been updated successfully.")
+                admin.execute(f"UPDATE customer_info SET {selected_field} = ? WHERE filmid = ?", (new_value, selected_record))
+                database.commit()
+                print(f"\nRecord {film_id} has been updated successfully.")
 
-        except sql.OperationalError as e:
-            database.rollback()
-            print(f"\nRecord not updated: {e}")
-            
-            
-    elif selected_table.lower() == "product":
-        try:
-            admin.execute(f"UPDATE tblfilms SET {field_name} = ? WHERE filmid = ?", (new_value, film_id))
-            database.commit()
-            print(f"\nRecord {film_id} has been updated successfully.")
+            except sql.OperationalError as e:
+                database.rollback()
+                print(f"\nRecord not updated: {e}")
+                break
+        
+        
+    # elif selected_table.lower() == "product":
+    #     try:
+    #         admin.execute(f"UPDATE tblfilms SET {field_name} = ? WHERE filmid = ?", (new_value, film_id))
+    #         database.commit()
+    #         print(f"\nRecord {film_id} has been updated successfully.")
 
-        except sql.OperationalError as e:
-            database.rollback()
-            print(f"\nRecord not updated: {e}")
+    #     except sql.OperationalError as e:
+    #         database.rollback()
+    #         print(f"\nRecord not updated: {e}")
             
             
 
